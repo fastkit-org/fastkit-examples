910a0ec84409980c396722e4c42ddc9ddcece804
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2026-04-28 21:49:55 +0200

    Setup cache

--- fastkit-invoicing/main.py
+++ fastkit-invoicing/main.py
@@ -4,9 +4,12 @@ from modules.products.router import router as products_router
 from modules.invoices.router import router as invoices_router
 
 from fastkit_core.database import init_async_database
+from fastkit_core.cache import setup_cache
 from fastkit_core.config import ConfigManager
-configuration = ConfigManager(modules=['app', 'database'])
+
+configuration = ConfigManager(modules=['app', 'database', 'cache'])
 init_async_database(configuration)
+setup_cache(configuration)
 
 app = FastAPI()
 
